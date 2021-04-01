ff86ddf31fc76e623299436bbc5b895ebb652fd0
choi-dongmin <57117748+choi-dongmin@users.noreply.github.com>
2021-04-02 03:05:36 +0900

    Add files via upload

---
+++ # 변수/Var.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Variable</h1>
+
+    <?php
+    $num = 2;     // PHP 는 변수 앞에 $ 표시를 입력한다.
+    print($num+1);
+    ?>
+
+  </body>
+</html>
